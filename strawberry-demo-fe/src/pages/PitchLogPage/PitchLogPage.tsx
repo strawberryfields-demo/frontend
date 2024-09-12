75879c5f7744f482d91e19defb286d3bca759ddd
nakyoung98 <jay3925@cau.ac.kr>
2024-09-12 12:28:07 +0900

    Feat: 루트에 해당하는 페이지 연결 및 관련 페이지 생성

---
+++ strawberry-demo-fe/src/pages/PitchLogPage/PitchLogPage.tsx
@@ -0,0 +1,3 @@
+export default function PitchLogPage() {
+  return <div>PitchLogPage</div>;
+}
--- strawberry-demo-fe/src/routes/Router.tsx
+++ strawberry-demo-fe/src/routes/Router.tsx
@@ -1,11 +1,15 @@
 import { PATHS } from "@/constants/paths";
 import HeroPage from "@/pages/HeroPage/HeroPage";
+import PitchLogPage from "@/pages/PitchLogPage/PitchLogPage";
+import SignInPage from "@/pages/SignInPage/SignInPage";
+import SignUpPage from "@/pages/SignUpPage/SignUpPage";
+import SongPage from "@/pages/SongPage/SongPage";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   { path: PATHS.HERO, element: <HeroPage /> },
-  { path: PATHS.SIGN_IN, element: <HeroPage /> },
-  { path: PATHS.SIGN_UP, element: <HeroPage /> },
-  { path: PATHS.SONG, element: <HeroPage /> },
-  { path: PATHS.PITCH_LOG, element: <HeroPage /> },
+  { path: PATHS.SIGN_IN, element: <SignInPage /> },
+  { path: PATHS.SIGN_UP, element: <SignUpPage /> },
+  { path: PATHS.SONG, element: <SongPage /> },
+  { path: PATHS.PITCH_LOG, element: <PitchLogPage /> },
 ]);
