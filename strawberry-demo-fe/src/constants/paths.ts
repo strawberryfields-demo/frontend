fcc1402174973e90df92cc033c5f00bb992cced3
nakyoung98 <jay3925@cau.ac.kr>
2024-09-12 12:58:34 +0900

    Chore: Dashboard Route 추가 및 관련 설정

--- strawberry-demo-fe/src/constants/paths.ts
+++ strawberry-demo-fe/src/constants/paths.ts
@@ -2,6 +2,7 @@ export const PATHS = {
   HERO: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  DASHBOARD: ":username",
   SONG: ":username/song",
   PITCH_LOG: ":username/pitch-log",
 };
--- strawberry-demo-fe/src/routes/Router.tsx
+++ strawberry-demo-fe/src/routes/Router.tsx
@@ -1,4 +1,5 @@
 import { PATHS } from "@/constants/paths";
+import DashboardPage from "@/pages/DashboardPage/DashboardPage";
 import HeroPage from "@/pages/HeroPage/HeroPage";
 import PitchLogPage from "@/pages/PitchLogPage/PitchLogPage";
 import SignInPage from "@/pages/SignInPage/SignInPage";
@@ -10,6 +11,9 @@ export const router = createBrowserRouter([
   { path: PATHS.HERO, element: <HeroPage /> },
   { path: PATHS.SIGN_IN, element: <SignInPage /> },
   { path: PATHS.SIGN_UP, element: <SignUpPage /> },
+
+  // user routes
+  { path: PATHS.DASHBOARD, element: <DashboardPage /> },
   { path: PATHS.SONG, element: <SongPage /> },
   { path: PATHS.PITCH_LOG, element: <PitchLogPage /> },
 ]);
