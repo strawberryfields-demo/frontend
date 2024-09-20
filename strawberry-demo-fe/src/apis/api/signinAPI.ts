8e53b90aa1c12b48aa16937a3d421ee9a01aebc6
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 17:59:37 +0900

    Chore: API_URLS 타입 변경에 따른 조정

--- strawberry-demo-fe/src/apis/api/signinAPI.ts
+++ strawberry-demo-fe/src/apis/api/signinAPI.ts
@@ -7,7 +7,8 @@ const { SIGN_IN } = API_URLS.SIGN_IN;
 export const signinAPI = async (data: SignInRequestDTO) => {
   return await apiCall<SignInRequestDTO, SignInResponseDTO>({
     method: "POST",
-    endpoint: SIGN_IN,
+    //TODO: 타입 단언 개선 필요
+    endpoint: SIGN_IN as string,
     data,
   });
 };
