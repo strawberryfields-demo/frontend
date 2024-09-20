459ea8adf5bcd82954ead8d91b36938dfebaebfb
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 03:34:53 +0900

    Feat: useSigninForm return Type 지정

--- strawberry-demo-fe/src/hooks/useForm/useSigninForm.ts
+++ strawberry-demo-fe/src/hooks/useForm/useSigninForm.ts
@@ -1,12 +1,21 @@
+import { UseFormReturn, UseFormRegister } from "react-hook-form";
 import _ from "lodash";
 import { SignInFormData } from "@/types/forms";
-import useForm from "./useForm";
 import { VALIDATION_RULES } from "@/constants/validation/validationRules";
+import useForm from "./useForm";
 
-export const useSigninForm = () => {
+type SignInFormRegister = {
+  [K in keyof SignInFormData]: ReturnType<UseFormRegister<SignInFormData>>;
+};
+
+export type UseSigninFormReturn = Omit<UseFormReturn<SignInFormData>, "register"> & {
+  registers: SignInFormRegister;
+};
+
+export const useSigninForm = (): UseSigninFormReturn => {
   const { register, ...formResult } = useForm<SignInFormData>();
 
-  const registers = {
+  const registers: SignInFormRegister = {
     email: register("email", _.pick(VALIDATION_RULES.email, ["required", "pattern"])),
     password: register("password", _.pick(VALIDATION_RULES.password, ["required"])),
   };
