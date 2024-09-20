871fa9ac6c5b67c09e7cb8481e4e0f3bb805dfcb
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 18:37:33 +0900

    Chore: Form Prop type 변경
    실제 Form Attribute 받음

--- strawberry-demo-fe/src/components/FormElement/Form.tsx
+++ strawberry-demo-fe/src/components/FormElement/Form.tsx
@@ -1,7 +1,13 @@
-type FormProps = {
+import { FormHTMLAttributes } from "react";
+
+type FormProps = FormHTMLAttributes<HTMLFormElement> & {
   children: React.ReactNode;
 };
 
-export default function Form({ children }: FormProps) {
-  return <form className="flex flex-col gap-3">{children}</form>;
+export default function Form({ children, ...formProps }: FormProps) {
+  return (
+    <form className="flex flex-col gap-3" {...formProps}>
+      {children}
+    </form>
+  );
 }
