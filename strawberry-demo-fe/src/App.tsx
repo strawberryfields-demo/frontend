f161bc57afb6a9f3e495cc99ad6e50523b868d91
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 15:08:03 +0900

    Feat: React dev tools 추가

--- strawberry-demo-fe/src/App.tsx
+++ strawberry-demo-fe/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
