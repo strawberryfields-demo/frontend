09970d85c2d2a393abd68943b03c07a923b82b29
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 00:01:02 +0900

    Feat: SignInLayout 및 SIgnInPage 구현

--- strawberry-demo-fe/src/layouts/SigninLayout/SigninLayout.tsx
+++ strawberry-demo-fe/src/layouts/SigninLayout/SigninLayout.tsx
@@ -1,9 +1,54 @@
+import { Loader2 } from "lucide-react";
+
+import Form from "@/components/FormElement/Form";
+import FormLabel from "@/components/FormElement/FormLabel";
+import Section from "@/components/PageLayout/Section";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+import { SignInFormData } from "@/types/forms";
+import ElementWithDivider from "@/components/Divider/ElementWithDivider";
+import { Link, useNavigate } from "react-router-dom";
+import { PAGE_PATHS } from "@/constants/paths";
+import { PageTitle } from "@/components/PageLayout/PageElement";
+
 type SigninLayoutProps = {
-  onSignin: (email: string, password: string) => void;
+  onSignin: (formData: SignInFormData) => void;
   isOnLogin: boolean;
 };
 
 export default function SigninLayout({ onSignin, isOnLogin }: SigninLayoutProps) {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>();
+
+  const onSubmit: SubmitHandler<SignInFormData> = (data) => onSignin(data);
+
   return (
+    <Section>
+      <PageTitle title={"로그인"} className="text-center" />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormLabel label="이메일" htmlFor="email">
+          <Input placeholder="이메일" {...register("email")} />
+        </FormLabel>
+        <FormLabel label="비밀번호" htmlFor="password">
+          <Input placeholder="비밀번호" {...register("password")} />
+        </FormLabel>
+        <Button disabled={isOnLogin}>
+          {isOnLogin && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          로그인
+        </Button>
+      </Form>
+      <div className="flex justify-center py-4">
+        <ElementWithDivider>
+          <Link to={PAGE_PATHS.SIGN_UP}>회원가입</Link>
+          <Link to={""}>비밀번호 찾기</Link>
+        </ElementWithDivider>
+      </div>
+    </Section>
   );
 }
--- strawberry-demo-fe/src/pages/SignInPage/SignInPage.tsx
+++ strawberry-demo-fe/src/pages/SignInPage/SignInPage.tsx
@@ -1,3 +1,11 @@
+import { PAGE_PATHS } from "@/constants/paths";
+import { useSignin } from "@/hooks/useReactQuery/useSignin";
+import SigninLayout from "@/layouts/SigninLayout/SigninLayout";
+import { SignInFormData } from "@/types/forms";
+import { ConvertSignInFormDataToRequestDTO } from "@/utils/adapter/signInAdapter";
+
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  const { signin, handleSignin } = useSignin<SignInFormData>(PAGE_PATHS.DASHBOARD, ConvertSignInFormDataToRequestDTO);
+
+  return <SigninLayout onSignin={handleSignin} isOnLogin={signin.isPending} />;
 }
