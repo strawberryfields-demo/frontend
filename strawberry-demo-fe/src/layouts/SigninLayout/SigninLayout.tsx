88a05a755473bfbb158f913dee09ff4efbec23f9
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 03:37:03 +0900

    Fix: SignInPage 구현

--- strawberry-demo-fe/src/layouts/SigninLayout/SigninLayout.tsx
+++ strawberry-demo-fe/src/layouts/SigninLayout/SigninLayout.tsx
@@ -5,67 +5,38 @@ import Section from "@/components/PageLayout/Section";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-import { useForm, SubmitHandler } from "react-hook-form";
 import { SignInFormData } from "@/types/forms";
 import ElementWithDivider from "@/components/Divider/ElementWithDivider";
 import { Link } from "react-router-dom";
 import { PAGE_PATHS } from "@/constants/paths";
 import { PageTitle } from "@/components/PageLayout/PageElement";
 import FormElement from "@/components/FormElement/FormElement";
+import { UseSigninFormReturn } from "@/hooks/useForm/useSigninForm";
 
 type SigninLayoutProps = {
-  onSignin: (formData: SignInFormData) => void;
+  onSubmit: (formData: SignInFormData) => void;
   isOnLogin: boolean;
+  signinFormResults: UseSigninFormReturn;
 };
 
-export default function SigninLayout({ onSignin, isOnLogin }: SigninLayoutProps) {
-  const {
-    register,
-    handleSubmit,
+export default function SigninLayout({
+  onSubmit,
+  isOnLogin,
+  signinFormResults: {
+    registers: { email, password },
     formState: { errors },
-    setError,
-  } = useForm<SignInFormData>({
-    reValidateMode: "onSubmit",
-    shouldFocusError: true,
-  });
-
-  const onSubmit: SubmitHandler<SignInFormData> = (data) => onSignin(data);
-
+    handleSubmit,
+  },
+}: SigninLayoutProps) {
   return (
     <Section>
       <PageTitle title={"로그인"} className="text-center" />
       <Form onSubmit={handleSubmit(onSubmit)}>
         <FormElement label="이메일" htmlFor="email" errorMessage={errors.email?.message}>
-          <Input
-            placeholder="이메일"
-            {...register("email", {
-              required: "이메일을 입력해주세요.",
-              pattern: {
-                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                message: "이메일 형식이 올바르지 않습니다.",
-              },
-            })}
-          />
+          <Input placeholder="이메일" isError={!!errors.email?.message} {...email} />
         </FormElement>
         <FormElement label="비밀번호" htmlFor="password" errorMessage={errors.password?.message}>
-          <Input
-            placeholder="비밀번호"
-            {...register("password", {
-              required: "비밀번호를 입력해주세요.",
-              // minLength: {
-              //   value: 8,
-              //   message: "비밀번호는 8자 이상이어야 합니다.",
-              // },
-              // maxLength: {
-              //   value: 20,
-              //   message: "비밀번호는 20자 이하여야 합니다.",
-              // },
-              // pattern: {
-              //   value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g,
-              //   message: "비밀번호는 영문 대소문자, 숫자를 포함해야 합니다.",
-              // },
-            })}
-          />
+          <Input placeholder="비밀번호" type="password" isError={!!errors.password?.message} {...password} />
         </FormElement>
         <Button disabled={isOnLogin}>
           {isOnLogin && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
--- strawberry-demo-fe/src/pages/SignInPage/SignInPage.tsx
+++ strawberry-demo-fe/src/pages/SignInPage/SignInPage.tsx
@@ -1,11 +1,23 @@
 import { PAGE_PATHS } from "@/constants/paths";
+import { useSigninForm } from "@/hooks/useForm/useSigninForm";
 import { useSignin } from "@/hooks/useReactQuery/useSignin";
 import SigninLayout from "@/layouts/SigninLayout/SigninLayout";
 import { SignInFormData } from "@/types/forms";
 import { ConvertSignInFormDataToRequestDTO } from "@/utils/adapter/signInAdapter";
+import { useEffect } from "react";
 
 export default function SignInPage() {
   const { signin, handleSignin } = useSignin<SignInFormData>(PAGE_PATHS.DASHBOARD, ConvertSignInFormDataToRequestDTO);
+  const signinFormResults = useSigninForm();
 
-  return <SigninLayout onSignin={handleSignin} isOnLogin={signin.isPending} />;
+  //에러 관리
+  useEffect(() => {
+    if (signin.error && signin.error.response) {
+      const { email, password } = signin.error.response.data;
+      email && signinFormResults.setError("email", { message: email[0] });
+      password && signinFormResults.setError("password", { message: password[0] });
+    }
+  }, [signin.error]);
+
+  return <SigninLayout onSubmit={handleSignin} isOnLogin={signin.isPending} signinFormResults={signinFormResults} />;
 }
