e84b6f364aa1420d6868aebced1001afc74dbcee
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 18:21:46 +0900

    Feat: SigninLayout 컴포넌트 생성

---
+++ strawberry-demo-fe/src/layouts/SigninLayout/SigninLayout.tsx
@@ -0,0 +1,9 @@
+type SigninLayoutProps = {
+  onSignin: (email: string, password: string) => void;
+  isOnLogin: boolean;
+};
+
+export default function SigninLayout({ onSignin, isOnLogin }: SigninLayoutProps) {
+  return (
+  );
+}
