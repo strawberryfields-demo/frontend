86b9fcb1c22aee02021f1b0e6fe5dd74a091734e
nakyoung98 <jay3925@cau.ac.kr>
2024-09-20 17:59:51 +0900

    Chore: QUERY_KEYS 상수 정의

---
+++ strawberry-demo-fe/src/constants/reactQuery.ts
@@ -0,0 +1,26 @@
+import { getReactQueryKeysFromURL } from "@/utils/reactQueryUtils";
+import { PAGE_PATH_KEYS } from "./paths";
+import { API_URLS } from "./urls";
+
+// Query Key 타입 정의
+type QUERY_KEY_TYPE = {
+  [Key in PAGE_PATH_KEYS]: {
+    [SubKey: string]: ((...args: any[]) => string[]) | string[];
+  };
+};
+
+export const QUERY_KEYS: QUERY_KEY_TYPE = {
+  HERO: {},
+  SIGN_IN: {
+    SIGN_IN: getReactQueryKeysFromURL(API_URLS["SIGN_IN"]["SIGN_IN"] as string),
+  },
+  SIGN_UP: {
+    SIGN_UP: getReactQueryKeysFromURL(API_URLS["SIGN_IN"]["SIGN_UP"] as string),
+  },
+  DASHBOARD: {},
+  SONG: {
+    GET_SONG: (songId: string) =>
+      getReactQueryKeysFromURL((API_URLS["SONG"]["GET_SONG"] as (id: string) => string)(songId)),
+  },
+  PITCH_LOG: {},
+};
