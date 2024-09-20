046c45a9730935f78588947450ce4f37d19f9c9c
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 04:41:50 +0900

    Fix: Input 컴포넌트 isError props에서 분리

--- strawberry-demo-fe/src/components/ui/input.tsx
+++ strawberry-demo-fe/src/components/ui/input.tsx
@@ -6,13 +6,13 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
   isError?: boolean;
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, isError, ...props }, ref) => {
   return (
     <input
       type={type}
       className={cn(
         "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-        props.isError && "border-red-400",
+        isError && "border-red-400",
         className,
       )}
       ref={ref}
