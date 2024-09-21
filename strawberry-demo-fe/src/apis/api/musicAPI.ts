b226b57ceb62bc42d6b175d8e37a14d227f3f73a
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 15:30:30 +0900

    Feat: uploadSongToS3ParallelAPI 구현
    Promise.all 사용

--- strawberry-demo-fe/src/apis/api/musicAPI.ts
+++ strawberry-demo-fe/src/apis/api/musicAPI.ts
@@ -1,12 +1,12 @@
 import { API_URLS } from "@/constants/urls";
-import { MusicUploadRequestAPI, MusicUploadResponseAPI } from "../dtos/musicDto";
+import { MusicUploadRequestDTO, MusicUploadResponseDTO } from "../dtos/musicDto";
 import apiCall from "../axios";
 import { AxiosHeaders } from "axios";
 
 const { UPLOAD_SONG } = API_URLS.SONG;
 
-export const uploadSongMetaDataAndGetS3URL = async (data: MusicUploadRequestAPI) =>
-  await apiCall<MusicUploadRequestAPI, MusicUploadResponseAPI>({
+export const uploadSongMetaDataAndGetS3URL = async (data: MusicUploadRequestDTO) =>
+  await apiCall<MusicUploadRequestDTO, MusicUploadResponseDTO>({
     method: "POST",
     endpoint: UPLOAD_SONG as string,
     data,
@@ -21,3 +21,10 @@ export const uploadSongToS3API = async (data: File, s3Endpoint: string) =>
       "Content-Type": data.type,
     }),
   });
+
+export const uploadSongToS3ParallelAPI = async (data: File[], s3Endpoints: string[]) =>
+  await Promise.all(
+    s3Endpoints.map((url, index) => {
+      return uploadSongToS3API(data[index], url);
+    }),
+  );
