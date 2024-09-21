c8a42bc61012870922e0fb67da23736671e28f8f
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 18:34:33 +0900

    Feat: UploadMusicModalContainer 업로드 완료 된 파일 삭제 로직 구현

--- strawberry-demo-fe/src/pages/SongPage/UploadMusicModalContainer.tsx
+++ strawberry-demo-fe/src/pages/SongPage/UploadMusicModalContainer.tsx
@@ -1,16 +1,25 @@
 import { useManageMultipleFile } from "@/hooks/useManageMultipleFile";
 import { useSongUpload } from "@/hooks/useReactQuery/useSong";
 import UploadMusicModal from "@/layouts/ModalLayout/UploadMusicModal";
+import { useEffect } from "react";
 
 export default function UploadMusicModalContainer() {
   const { fileList: musicList, addFile: handleMusicChange, removeFile: handleMusicRemove } = useManageMultipleFile();
-  const { handleMusicUpload, isUploading, songUpload, songUploadToS3Parallel } = useSongUpload();
+  const { handleMusicUpload, isUploading, uploadSuccessStatuses } = useSongUpload();
 
   const handleMusicDelete = (selectedRows: File[]) => {
     selectedRows.forEach((file) => {
       handleMusicRemove(file);
     });
   };
+
+  useEffect(() => {
+    uploadSuccessStatuses.forEach((status, index) => {
+      if (status) {
+        handleMusicRemove(musicList[index]);
+      }
+    });
+  }, [uploadSuccessStatuses]);
 
   return (
     <UploadMusicModal
@@ -20,6 +29,7 @@ export default function UploadMusicModalContainer() {
       onUpload={() => {
         handleMusicUpload(musicList);
       }}
+      isUploading={isUploading}
     />
   );
 }
