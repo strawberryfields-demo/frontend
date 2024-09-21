214a02cf265c08551fd3fdf85c9fac983b37cd1e
nakyoung98 <jay3925@cau.ac.kr>
2024-09-21 13:30:46 +0900

    Chore: SignInFormRegister, UseSigninFormReturn 범용성있게 변경

--- strawberry-demo-fe/src/hooks/useForm/useSigninForm.ts
+++ strawberry-demo-fe/src/hooks/useForm/useSigninForm.ts
@@ -1,21 +1,12 @@
-import { UseFormReturn, UseFormRegister } from "react-hook-form";
 import _ from "lodash";
-import { SignInFormData } from "@/types/forms";
+import { FormRegister, FormReturn, SignInFormData } from "@/types/forms";
 import { VALIDATION_RULES } from "@/constants/validation/validationRules";
 import useForm from "./useForm";
 
-type SignInFormRegister = {
-  [K in keyof SignInFormData]: ReturnType<UseFormRegister<SignInFormData>>;
-};
-
-export type UseSigninFormReturn = Omit<UseFormReturn<SignInFormData>, "register"> & {
-  registers: SignInFormRegister;
-};
-
-export const useSigninForm = (): UseSigninFormReturn => {
+export const useSigninForm = (): FormReturn<SignInFormData> => {
   const { register, ...formResult } = useForm<SignInFormData>();
 
-  const registers: SignInFormRegister = {
+  const registers: FormRegister<SignInFormData> = {
     email: register("email", _.pick(VALIDATION_RULES.email, ["required", "pattern"])),
     password: register("password", _.pick(VALIDATION_RULES.password, ["required"])),
   };
