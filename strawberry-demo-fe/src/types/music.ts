ba3dfb959d3a2b497775dcd7eb22451c8931e1d7
nakyoung98 <jay3925@cau.ac.kr>
2024-09-23 04:42:00 +0900

    Fix: Music 모델 내부 프로퍼티 일부 수정

--- strawberry-demo-fe/src/types/music.ts
+++ strawberry-demo-fe/src/types/music.ts
@@ -1,12 +1,12 @@
 export type Music = {
   id: string;
-  name: string;
+  title: string;
   artist: string;
   album?: string;
   duration: number;
   genre?: string;
   releaseDate: string;
-  musicUrl: string;
+  file_path: string;
 };
 
 export type AllowedMusicExtension = "mp3" | "wav";
